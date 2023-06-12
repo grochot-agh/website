22667d9005a79bdf91d5eb5afe4f70760144c3de
Wiki <wjanicka@student.agh.edu.pl>
2023-06-12 16:44:59 +0200

    Add Users Model, Context and basic Controller

---
+++ Models/User.cs
@@ -0,0 +1,12 @@
+namespace final_project.Models;
+//prykładowy model
+public class User
+{
+    public long ID { get; set; }
+    public string? Name { get; set; }
+    public string? Surname { get; set;}
+    public string? Email { get; set;}
+    public int Age{ get; set; }
+    public string? Password { get; set; }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using final_project.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<UserContext>(opt =>opt.UseInMemoryDatabase("UserList"));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
